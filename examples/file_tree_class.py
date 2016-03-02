040d28a0ac75bec7ce2710998f8f43ffad4c4bd0
francisparan <francisparan@gmail.com>
2016-03-02 09:48:32 -0500

    added display of the number of directories and files in the directory on which file_tree is being invoked

--- examples/file_tree_class.py
+++ examples/file_tree_class.py
@@ -13,15 +13,12 @@ class Tree:
     def __init__(self, depth=0, line_by_line=0):
         self.depth = depth
         self.line_by_line = line_by_line
-        self.__end = '\n'
+        self.__end = '' if line_by_line else '\n'
         self.level = 1
         self.files = 0
         self.directories = 0
 
     def get_files(self, directory, show_hidden=False):
-
-        if not os.path.isdir(directory):
-            raise Exception('Directory not found: %s' % directory)
 
         files = os.listdir(directory)
 
@@ -30,40 +27,48 @@ class Tree:
     def display_file(self, file_name, padding=0):
         pad = ''.rjust(padding)
         file_output = "{} {}".format('-'.rjust(3), file_name)
-        print(pad + file_output, end = self.__end)
+        print(pad + file_output, end=self.__end)
 
     def print_tree(self, directory='.', show_hidden=False, recurse=False, level=1):
 
         indent = 5 * (level-1)
-        self.files += 1
+        recurse = self.depth or recurse
 
         for f in self.get_files(directory, show_hidden):
 
-            if self.line_by_line:
-                self.__end = ' '
-            
             self.display_file(f, padding=indent)
 
             if self.line_by_line and raw_input('').lower() == 'q':
-                raise Exception
+                raise SystemExit
 
             path = os.path.join(directory, f)
 
-            if recurse and (os.path.isdir(path) and os.access(path, os.R_OK)):
+            if os.path.isdir(path):
+                self.directories += 1
+
                 if self.depth and level >= self.depth:
                     continue
-                self.print_tree(path, show_hidden=show_hidden, recurse=recurse, level=level + 1)
+                elif recurse and os.access(path, os.R_OK):
+                    self.print_tree(path, show_hidden=show_hidden, recurse=recurse, level=level + 1)
+            else:
+                self.files += 1
 
 
 def tree(directory='.', show_hidden=False, recurse=False, line_by_line=False, depth=0):
-    if not os.path.isdir(directory):
-        if os.path.isfile(directory):
-            raise Exception('%s is a file' % directory)
+    if os.path.isfile(directory):
+        raise Exception('%s is a file' % directory)
+    elif not os.path.isdir(directory):
         raise Exception('Directory not found: %s' % directory)
 
+    obj = Tree(depth, line_by_line)
     print(directory)
-    Tree(depth, line_by_line).print_tree(directory, show_hidden, recurse)
 
+    try:
+        obj.print_tree(directory, show_hidden, recurse)
+
+        print("{} directories {} files".format(obj.directories, obj.files))
+    except:
+        print("stopped...")
 
 if __name__ == '__main__':
     tree()
